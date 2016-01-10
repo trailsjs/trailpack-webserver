e5ceac79db957c7a2e465eb799e3bd2b6a78df72
Travis Webb <me@traviswebb.com>
2016-01-09 19:05:30 -0500

    [wip] initial implementation

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const _ = require('lodash')
+const Trailpack = require('trailpack')
+
+const footprintOptions = [
+  'populate',
+  'limit',
+  'offset'
+]
+
+/**
+ * Web Server Trailpack
+ *
+ * Web Servers should inherit from this Trailpack in order to provide consistent
+ * API for all web servers.
+ */
+module.exports = class WebServerTrailpack extends Trailpack {
+
+  /**
+   * Extract options from request query and return the object subset.
+   */
+  getOptionsFromQuery (query) {
+    return _.pick(query, footprintOptions)
+  }
+
+  /**
+   * Extract the criteria from the query
+   */
+  getCriteriaFromQuery (query) {
+    return _.omit(query, footprintOptions)
+  }
+  
+}
