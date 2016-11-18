0b4229bd8ca8849610c729b4051dca54754251ce
Jimmy Aumard <jaumard@nestle.com>
2016-11-18 07:37:50 +0100

    Parse query params

--- index.js
+++ index.js
@@ -16,21 +16,38 @@ const footprintOptions = [
  */
 module.exports = class WebServerTrailpack extends Trailpack {
 
+  _parseQuery(data) {
+    return _.mapValues(data, value => {
+      if (value === 'true' || value === 'false') {
+        value = value === 'true'
+      }
+
+      if (value === '%00' || value === 'null') {
+        value = null
+      }
+      const parseValue = parseFloat(value)
+      if (!isNaN(parseValue)) {
+        value = parseValue
+      }
+      return value
+    })
+  }
+
   /**
    * Extract options from request query and return the object subset.
    */
-  getOptionsFromQuery (query) {
-    return _.pick(query, footprintOptions)
+  getOptionsFromQuery(query) {
+    return this._parseQuery(_.pick(query, footprintOptions))
   }
 
   /**
    * Extract the criteria from the query
    */
-  getCriteriaFromQuery (query) {
-    return _.omit(query, footprintOptions)
+  getCriteriaFromQuery(query) {
+    return this._parseQuery(_.omit(query, footprintOptions))
   }
 
-  constructor (app, config) {
+  constructor(app, config) {
     if (!config) {
       throw new Error('trailpack-webserver must be subclassed. Do not load it directly.')
     }
